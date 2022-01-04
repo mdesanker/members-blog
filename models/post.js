551bd42dc0e17125a7ca9cb90ac4ca3925f6b177
Michael Desanker <mdesanker@gmail.com>
2022-01-04 05:34:45 -0500

    Add post model

---
+++ models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, required: true },
+  date: { type: Date, default: Date.now() },
+  content: { type: String, required: true },
+});
+
+// Virtual for post url
+PostSchema.virtual("url").get(function () {
+  return "/post/" + this._id;
+});
+
+module.exports = mongoose.model("Post", PostSchema);
