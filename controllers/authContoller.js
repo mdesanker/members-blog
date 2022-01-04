e87232f378472c345b566a1cbce841ab577d89ad
Michael Desanker <mdesanker@gmail.com>
2022-01-04 07:14:42 -0500

    Set up sign up post controller

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 const async = require("async");
 const { body, validationResult } = require("express-validator");
 
@@ -7,9 +9,52 @@ exports.signupGet = function (req, res, next) {
 };
 
 // Display sign up on POST
-exports.signupPost = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: Sign up POST");
-};
+exports.signupPost = [
+  // Validate and sanitize form inputs
+  body("username", "Username required").trim().isLength({ min: 1 }).escape(),
+  body("password", "Password required")
+    .trim()
+    .isLength({ min: 7 })
+    .withMessage("Password must be at least 7 characters")
+    .escape(),
+  body("confirmPassword", "Must have same value as password field").custom(
+    (value, { req }) => value === req.body.password
+  ),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Create a new user object with processed inputs
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+      membership: false,
+    });
+
+    if (!errors.isEmpty()) {
+      // Render form again if errors
+      res.render("signup", { title: "Sign Up", errors: errors.array() });
+      return;
+    } else {
+      // Inputs are valid
+      // Check if user already exists
+      User.findOne({ username: req.body.username }).exec(function (
+        err,
+        foundUser
+      ) {
+        if (err) next(err);
+        if (foundUser) res.redirect("/");
+
+        // Save new user
+        user.save(function (err) {
+          if (err) next(err);
+          res.redirect("/auth/login");
+        });
+      });
+    }
+  },
+];
 
 // Display log in form on GET
 exports.loginGet = function (req, res, next) {
--- models/user.js
+++ models/user.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  firstName: { type: String, required: true, maxlength: 50 },
-  lastName: { type: String, required: true, maxlength: 50 },
+  firstName: { type: String, maxlength: 50 },
+  lastName: { type: String, maxlength: 50 },
   email: { type: String },
   username: { type: String, required: true },
   password: { type: String, required: true, minlength: 7 },
-  membership: { type: Boolean, default: false, required: true },
+  membership: { type: Boolean, default: false },
   posts: [{ type: Schema.Types.ObjectId }],
 });
 
