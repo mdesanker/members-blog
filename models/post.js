c2c8393ed9a81595c2d2cb7650d1ff142e90edcf
Michael Desanker <mdesanker@gmail.com>
2022-01-05 07:59:53 -0500

    Add user ref to post model to populate authors

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   date: { type: Date, default: Date.now() },
   content: { type: String, required: true },
 });
--- models/user.js
+++ models/user.js
@@ -8,7 +8,6 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true, minlength: 7 },
   membership: { type: Boolean, default: false },
-  posts: [{ type: Schema.Types.ObjectId }],
 });
 
 // Virtual for full name
